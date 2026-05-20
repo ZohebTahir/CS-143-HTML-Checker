1d96c0e0afac72fdf84ffc4f6a3c5d27e89aaed0
OwenTjandra <owen.dunateo@gmail.com>
2026-05-20 11:22:25 -0700

    Return queue method completed.

--- HTMLManager.java
+++ HTMLManager.java
@@ -4,13 +4,16 @@ public class HTMLManager {
   private Queue<HTMLTag> tags;
   
   public HTMLManager(Queue<HTMLTag> html) {
-   if(html == null) {
-      throw new IllegalArgumentException(); 
-   } else {
-      while(!html.isEmpty()) {
-         tags.add(html.remove()); 
+      if (html == null) {
+         throw new IllegalArgumentException();
+      } else {
+      while(!html.isEmpty()){
+         tag.add(html.remove());
       }
-   }
-   
+        }      
+  }
+  
+  public Queue<HTMLTag> getTags() {
+      return tags;
   }
 }
