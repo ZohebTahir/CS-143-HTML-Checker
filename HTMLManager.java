48799d2d3a65158fe95108a7a6a034291c72c564
ZohebTahir <zohebt.dev@gmail.com>
2026-05-20 11:25:21 -0700

    getTags() correction

--- HTMLManager.java
+++ HTMLManager.java
@@ -4,16 +4,16 @@ public class HTMLManager {
   private Queue<HTMLTag> tags;
   
   public HTMLManager(Queue<HTMLTag> html) {
-      if (html == null) {
-         throw new IllegalArgumentException();
-      } else {
-      while(!html.isEmpty()){
-         tag.add(html.remove());
+   if(html == null) {
+      throw new IllegalArgumentException(); 
+   } else {
+      while(!html.isEmpty()) {
+         tags.add(html.remove()); 
       }
-        }      
+   }
+   
   }
-  
-  public Queue<HTMLTag> getTags() {
-      return tags;
-  }
+   public Queue<HTMLTag> getTags() {
+      return tags;   
+   }
 }
