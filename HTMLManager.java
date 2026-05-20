779fb1d7f08eae4c4c41b0bf49733c7c7cdd2673
OwenTjandra <owen.dunateo@gmail.com>
2026-05-20 11:15:03 -0700

    Added thrownotfound exception

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,4 +3,9 @@ import java.util.*;
 public class HTMLManager {
   private Queue<HTMLTag> tags;
   
+  public HTMLManager(Queue<HTMLTag> html) {
+      if (html == null) {
+         throw new IllegalArgumentException();
+      }        
+  }
 }
