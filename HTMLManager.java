3f4f62a69b1f2eaa7b894a1d471d3e8d29701e58
ZohebTahir <zohebt.dev@gmail.com>
2026-05-21 14:49:55 -0700

    fixHTML() logic complete with bugs

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,18 +1,19 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
+   private Queue<HTMLTag> tags;
   
-  public HTMLManager(Queue<HTMLTag> html) {
-   if(html == null) {
-      throw new IllegalArgumentException(); 
-   } else {
-      while(!html.isEmpty()) {
-         tags.add(html.remove()); 
+   public HTMLManager(Queue<HTMLTag> html) {
+      tags = new LinkedList<>(); 
+      if(html == null) {
+         throw new IllegalArgumentException(); 
+      } else {
+         while(!html.isEmpty()) {
+            tags.add(html.remove()); 
+         }
       }
-   }
    
-  }
+   }
    public Queue<HTMLTag> getTags() {
       return tags;   
    }
@@ -20,24 +21,39 @@ public class HTMLManager {
    public String toString() {
       String str = "";
       for(HTMLTag t: tags) {
-        str += t.toString();  
+         str += t.toString().trim();  
       }
       return str; 
    }
    
    public void fixHTML() {
-      Stack<String> stack = new Stack<>(); 
-      HTMLTag val = tags.remove();
-      if(val.isSelfClosing()) {
-         tags.add(val);  
-      } 
+      Stack<HTMLTag> stack = new Stack<>();
+      int size = tags.size();
+      for(int i = 0; i < size; i++) { 
+         HTMLTag val = tags.remove();
+         if(val.isSelfClosing()) {
+            tags.add(val);  
+         } 
          else if(val.isOpening()) { //is true
-            stack.push(val); 
+            stack.push(val);
+            tags.add(val);  
          
-      } else { //isClosing Scenario
-         
+         } else if(val.isClosing()){ //isClosing Scenario
+            //HTMLTag stackPeek = stack.peek();
+            if(val.matches(stack.peek()) && !stack.isEmpty()) {
+               tags.add(val);
+               stack.pop();  
+            }  else if(!stack.isEmpty()){
+               tags.add(val); 
+            } 
+            else {
+               HTMLTag matchFix = val.getMatching();
+               tags.add(matchFix);
+               stack.pop();   
+            }
+                  
+         }
       }
-      
    }
    
 }
